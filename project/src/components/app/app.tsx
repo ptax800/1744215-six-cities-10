4d220d8563b0e31cd11b3501f6fd97f10a4def90
Alexey Klevets <nazzzgul30@gmail.com>
2022-07-14 22:03:03 +0300

    1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,6 @@
+import LoginScreen from "../../pages/login-screen/login-screen";
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return <LoginScreen></LoginScreen>;
 }
 
 export default App;
