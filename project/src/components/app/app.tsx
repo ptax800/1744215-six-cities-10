da7e4542c1529da9679845e6d0e3939505d45e4e
Alexey Klevets <nazzzgul30@gmail.com>
2022-07-18 15:32:50 +0300

    сделал домашнее задание

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,6 +1,18 @@
-import LoginScreen from "../../pages/login-screen/login-screen";
+import LoginScreen from '../../pages/login-screen/login-screen';
+import MainScreen from '../../pages/Main/main-screen';
+import FavoriteEmptyScreen from '../../pages/Favorite-empty-screen/favorite-empty-screen';
+import Property from '../../pages/Property/Property';
+import PropertyNotLogged from '../../pages/property-not-logged/property-not-logged';
 function App(): JSX.Element {
-  return <LoginScreen></LoginScreen>;
+  return (
+    <>
+      <LoginScreen />
+      <MainScreen />
+      <FavoriteEmptyScreen />
+      <Property />
+      <PropertyNotLogged />
+    </>
+  );
 }
 
 export default App;
